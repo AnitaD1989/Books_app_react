a48450f0067b095b4064f0467a8cd22d04b408aa
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-07-16 14:58:05 +0200

    add redux

---
+++ src/components/BookForm/BookForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+const BookForm = () => {
+
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_BOOK', payload: { title, author}});;
+    setTitle('');
+    setAuthor('');
+  }
+
+  
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      Title: <input type="type" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      Author: <input type="text" value={author}  onChange={(e) => setAuthor(e.target.value)}/>
+      <button>Add book</button>
+    </form>
+  );
+};
+
+export default BookForm;
